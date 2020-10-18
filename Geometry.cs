93fc9ba4158c0184b1bfe6b946a51ce546918a81
Алексей Бринкевич <alesha20113@mail.ru>
2020-10-18 18:25:26 +0300

    Start

---
+++ Geometry.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+
+namespace VectorAndPolygonMath
+{
+    static class Geometry
+    {
+        static private Vector2D MinDistansePointLineSigment(Vector2D a, Vector2D b, Vector2D point) //ближайшая точка на отрезке к точке point
+        {
+            Vector2D l;
+            l = a - b;
+            float t = Vector2D.Dot(a - point, l) / l.SqrLength;
+            if      (t <= 0) return a;
+            else if (t >= 1) return b;
+            else /*0> t <1*/ return a - l * t;
+        }
+        static public float MinDistansePointСontour(Vector2D[] vertex, Vector2D point, out Vector2D closestVectorPoly) //ближайшая точка на контуре многоугольника vertex к точке point, возвращает расстояние между ними
+        {
+            Vector2D closestVectorSegment;
+            closestVectorPoly = MinDistansePointLineSigment(vertex[vertex.Length], vertex[0], point);
+            float Rmin = Distance(closestVectorPoly, point);
+            float R;
+            for (int i = 1; i < vertex.Length; i++)
+            {
+                closestVectorSegment = MinDistansePointLineSigment(vertex[i - 1], vertex[i], point);
+                R = Distance(closestVectorSegment, point);
+                if (R < Rmin)
+                {
+                    Rmin = R;
+                    closestVectorPoly = closestVectorSegment;
+                }
+            }
+            return Rmin;
+        }
+        static public float Distance(Vector2D a, Vector2D b) //расстояние между двумя точками
+        {
+            return (b - a).Length;
+        }
+
+        static private float LineSide(Vector2D a, Vector2D b, Vector2D point) //по какую сторону находится точка point от прямой (a, b)(смотреть по знаку возвращаемого числа)
+        {
+            return (a.X - point.X) * (b.Y - a.Y) - (b.X - a.X) * (a.Y - point.Y);
+        }
+
+        static public bool InPoly(Vector2D[] vertex, Vector2D point) //находится ли точка point внутри выпуклого многоугольника. Точки многоугольника строго по часовой стрелке!!!
+        {
+            if (vertex.Length < 3) throw new Exception("Polygon must have over 2 points");
+
+            if (LineSide(vertex.Last(), vertex[0], point) >= 0) 
+                return false;
+
+            for (int i = 1; i < vertex.Length; i++)
+                if (LineSide(vertex[i - 1], vertex[i], point) < 0) 
+                    return false;
+
+            return true;
+        }
+
+        static public bool IntersectionOfLineAndLineSigment(Vector2D line1, Vector2D line2, Vector2D lineSigment1, Vector2D lineSigment2) //пресекаются ли прямая (line1, line2) и отрезок (lineSigment1, lineSigment2)
+        {
+            float t = (line1 - line2) % (lineSigment1 - lineSigment2);
+            if (t == 0) 
+                return false;
+
+            Vector2D P = new Vector2D(line1 % line2 * (lineSigment1.X - lineSigment2.X) - lineSigment1 % lineSigment2 * (line1.X - line2.X), line1 % line2 * (lineSigment1.Y - lineSigment2.Y) - lineSigment1 % lineSigment2 * (line1.Y - line2.Y));
+            const float esp = 0.0001f;//не помню зачем это тут, написано кровью
+            P /= t;
+            if (((P.X <= lineSigment1.X + esp && P.X >= lineSigment2.X - esp) || 
+                (P.X >= lineSigment1.X - esp && P.X <= lineSigment2.X + esp)) 
+                && 
+                ((P.Y <= lineSigment1.Y + esp && P.Y >= lineSigment2.Y - esp) || 
+                (P.Y >= lineSigment1.Y - esp && P.Y <= lineSigment2.Y + esp)))
+                return true;
+            return false;
+        }
+        static public float LinePointDistance(Vector2D a, Vector2D b, Vector2D point) // расстояние от точки point до прямой (a,b)
+        {
+            return Math.Abs(point % (b - a) + b % a) / Distance(a, b);
+        }
+    }
+}
---
+++ Vector2D.cs
@@ -0,0 +1,161 @@
+﻿using System;
+
+namespace VectorAndPolygonMath
+{
+    class Vector2D
+    {
+        #region Incaps
+        bool notActualLenght;
+        bool notActualSqrLenght;
+        private float x;
+        private float y;
+        private float sqrLenght;
+        private float lenght;
+        #endregion
+        public float X
+        {
+            get => x; 
+            set
+            {
+                notActualLenght = true;
+                notActualSqrLenght = true;
+                x = value;
+            }
+        }
+        public float Y
+        {
+            get => y; 
+            set
+            {
+                notActualLenght = true;
+                notActualSqrLenght = true;
+                y = value;
+            }
+        }
+        public float SqrLength
+        {
+            get
+            {
+                if (notActualSqrLenght)
+                    UpdateSqrLength();
+                return sqrLenght;
+            }
+        }
+        public float Length
+        {
+            get
+            {
+                if (notActualLenght)
+                    UpdateLength();
+
+                return lenght;
+            }
+            set
+            {
+                if (notActualLenght)
+                    UpdateLength();
+
+                if (lenght < 0.0001f)
+                    X = Y = float.MaxValue;
+
+                X /= lenght * value;
+                Y /= lenght * value;
+            }
+        }
+        
+        public Vector2D(float x, float y)
+        {
+            X = x; 
+            Y = y;
+        }
+        
+        private void UpdateLength()
+        {
+            lenght = (float)Math.Sqrt(sqrLenght);
+            notActualLenght = false;
+        }
+        private void UpdateSqrLength()
+        {
+            sqrLenght = X * X + Y * Y;
+            notActualLenght = false;
+        }
+
+        #region operators
+        static public Vector2D operator +(Vector2D Vector1, Vector2D Vector2)
+        {
+            return new Vector2D(Vector1.X + Vector2.X, Vector1.Y + Vector2.Y);
+        }
+        static public Vector2D operator -(Vector2D Vector)
+        {
+            return new Vector2D(-Vector.X, -Vector.Y);
+        }
+        static public Vector2D operator -(Vector2D Vector1, Vector2D Vector2)
+        {
+            return new Vector2D(Vector1.X - Vector2.X, Vector1.Y - Vector2.Y);
+        }
+        static public Vector2D operator *(Vector2D Vector1, Vector2D Vector2)
+        {
+            return new Vector2D(Vector1.X * Vector2.X, Vector1.Y * Vector2.Y);
+        }
+        static public Vector2D operator *(Vector2D Vector1, float valuev)
+        {
+            return new Vector2D(Vector1.X * valuev, Vector1.Y * valuev);
+        }
+        static public Vector2D operator *(float valuev, Vector2D Vector1)
+        {
+            return Vector1 * valuev;
+        }
+        static public Vector2D operator /(Vector2D Vector1, Vector2D Vector2)
+        {
+            return new Vector2D(Vector1.X / Vector2.X, Vector1.Y / Vector2.Y);
+        }
+        static public Vector2D operator /(Vector2D Vector1, float valuev)
+        {
+            return new Vector2D(Vector1.X / valuev, Vector1.Y / valuev);
+        }
+        static public bool operator ==(Vector2D Vector1, Vector2D Vector2)
+        {
+            return (Vector1.X == Vector2.X) && (Vector1.X == Vector2.X);
+        }
+        static public bool operator !=(Vector2D Vector1, Vector2D Vector2)
+        {
+            return (Vector1.X != Vector2.X) || (Vector1.X != Vector2.X);
+        }
+
+        public override bool Equals(object o)
+        {
+            if (o == null)
+                return false;
+            return true;
+        }
+
+        public override int GetHashCode() { return 0; }
+        static public float operator %(Vector2D vec1, Vector2D vec2)
+        {
+            return vec1.X * vec2.Y - (vec1.Y * vec2.X);
+        }
+        static public Vector2D operator %(float a, Vector2D vec)
+        {
+            return new Vector2D(-a * vec.Y, vec.X * a);
+        }
+        static public Vector2D operator %(Vector2D vec, float a)
+        {
+            return new Vector2D(a * vec.Y, -vec.X * a);
+        }
+
+        #endregion
+
+        static public float Dot(Vector2D vec1, Vector2D vec2)
+        {
+            return vec1.X * vec2.X + vec1.Y * vec2.Y;
+        }
+
+        public Vector2D GetCuted()
+        {
+            if (Length < 0.0001) 
+                return new Vector2D(float.MaxValue, float.MaxValue);
+
+            return new Vector2D(X / Length, Y / Length);
+        }
+    }
+}
